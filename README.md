67fd888ca7c34afba95320b9d3dd5912d0378091
youlweb <julientord@gmail.com>
2015-02-20 08:20:18 -0800

    _Whitespace_ link.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-#_String_
-This library is under development.
+#\_String\_
+###Library of string links for \_Chain\_.
+
+-[\_Whitespace\_](/src/_Whitespace.php_)
+Any sequence of more than one whitespace is reduced to a single whitespace.
+By default, leading and trailing whitespaces are also removed.  
+Whitespaces include tab, newline, and linebreak characters.
---
+++ src/_Whitespace_.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * _Chain_ ©2015 Julien <youlweb@hotmail.com>
+ * Refer to the LICENSE file for the full copyright and license information.
+ * @package chain/string
+ */
+
+namespace _Chain_\_String_;
+
+use _Chain_\_AbsLink_;
+use _Chain_\I_O;
+use _Chain_\Type;
+
+/**
+ * Remove extra whitespaces from a string.
+ *
+ * Any sequence of more than one whitespace is reduced to a single whitespace.
+ * By default, leading and trailing whitespaces are also removed.
+ * Whitespaces include tab, newline, and linebreak characters.
+ *
+ * I/O contract
+ * ------------
+ * <pre>
+ * I    string      String to process.
+ * O    string      Processed string.
+ * X    no
+ * </pre>
+ *
+ * @author Julien Tord <youlweb@hotmail.com>
+ */
+class _Whitespace_ extends _AbsLink_
+{
+    /**
+     * @var bool
+     */
+    private $_trim;
+
+    /**
+     * @param bool $trim Remove leading/trailing whitespace as well.
+     */
+    public function __construct($trim = true)
+    {
+        $this->_trim = $trim;
+    }
+
+    /** {@inheritDoc} */
+    public function EXE(I_O $IO)
+    {
+        $filtered = preg_replace("/[\\x00-\\x20]+/", ' ', $IO->I_(Type::STRING));
+        if ($this->_trim) {
+            $filtered = trim($filtered);
+        }
+        return $IO->_O($filtered);
+    }
+}
