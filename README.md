c3be4399175f33caaf04c82df750f988831d9e00
youlweb <julientord@gmail.com>
2015-02-20 08:51:54 -0800

    _Whitespace_ tests.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 #\_String\_
-###Library of string links for \_Chain\_.
+###*Library of string links for \_Chain\_.*
 
--[\_Whitespace\_](/src/_Whitespace.php_)
+* [\_Whitespace\_](/src/_Whitespace_.php)
 Any sequence of more than one whitespace is reduced to a single whitespace.
 By default, leading and trailing whitespaces are also removed.  
 Whitespaces include tab, newline, and linebreak characters.
+
---
+++ Tests/_Whitespace_Test.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * _Chain_ ©2015 Julien <youlweb@hotmail.com>
+ * Refer to the LICENSE file for the full copyright and license information.
+ * @package chain/string
+ */
+
+namespace _Chain_\_String_;
+use _Chain_\Type;
+
+/**
+ * @author Julien Tord <youlweb@hotmail.com>
+ */
+class _Whitespace_Test extends \PHPUnit_Framework_TestCase
+{
+    const IO = '\_Chain_\I_O';
+
+    public function testEXE()
+    {
+        $IO = $this->getMockIO();
+        $IO->expects($this->once())->method('I_')->with(Type::STRING)
+            ->willReturn("  \t\t   \n \n  \r \r foo  \n  bar \t\t  \n");
+        $IO->expects($this->once())->method('_O')->with('foo bar');
+        $whitespace = new _Whitespace_();
+        $whitespace->EXE($IO);
+    }
+
+    public function testEXENoTrim()
+    {
+        $IO = $this->getMockIO();
+        $IO->expects($this->once())->method('I_')->with(Type::STRING)
+            ->willReturn(" \n\n  foo    bar  \t \n");
+        $IO->expects($this->once())->method('_O')->with(' foo bar ');
+        $whitespace = new _Whitespace_(false);
+        $whitespace->EXE($IO);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getMockIO()
+    {
+        return $this->getMock(self::IO);
+    }
+}
